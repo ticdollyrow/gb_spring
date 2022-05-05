190d743cb3b43c51421e37cafd1c32e20411c163
Kate <frauaja@yandex.ru>
2022-05-05 21:44:59 +0300

    lesson5

--- Hibernate/ru.geekbrains.hibernateOne/src/main/resources/import.sql
+++ Hibernate/ru.geekbrains.hibernateOne/src/main/resources/import.sql
@@ -1,8 +1,3 @@
 DROP TABLE IF EXISTS  product;
 CREATE TABLE IF NOT EXISTS product (id bigserial, cost int, title VARCHAR(255), PRIMARY KEY (id));
-INSERT INTO product (title, cost)
-VALUES ('Orange', 13);
-INSERT INTO product (title, cost)
-VALUES ('Milk', 4);
-INSERT INTO product (title, cost)
-VALUES ('Apple', 12);
+INSERT INTO product (title, cost) VALUES ('Orange', 80), ('Milk', 80), ('Brot', 80);
